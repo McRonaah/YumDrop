a5350fd0c61a080bf694ac1f9a4c74921914e38f
McRonaah <ronaldcheruiyot342@gmail.com>
2023-03-22 12:52:37 +0300

    :arrow_up: setting up dependencies and packages :white_check_mark:

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="w-screen h-screen flex items-center justify-center text-blue-600"  >App</div>
   );
 }
 
-export default App
+export default App;
