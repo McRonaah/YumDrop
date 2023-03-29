d9b0a948f0ec891e107f74772523219a444856d0
McRonaah <ronaldcheruiyot342@gmail.com>
2023-03-29 11:39:53 +0300

    :rotating_light: updated firebase app inside src

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,22 @@ import Logo from '../img/logo.png';
 import { MdShoppingBasket } from "react-icons/md";
 import { motion } from 'framer-motion';
 
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { app } from "../firebase.config";
+
+
 import Avatar from "../img/avatar.png";
 import { Link } from 'react-router-dom';
 
 
 const Header = () => {
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const login = async () => {
+   const response = await signInWithPopup(firebaseAuth, provider);
+   console.log(response);
+  };
   return (
     <header className="fixed z-50 w-screen p-6 px-16"> 
             {/* large screen */}
@@ -49,20 +60,16 @@ const Header = () => {
                       <p className="text-xs text-white font-semibold">2</p>
                     </div>
                 </div>
-                <motion.img 
-            whileTap={{ scale: 0.6 }}
-            src={Avatar} 
-            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full" 
-            alt="userprofile" />
+              <div className="relative">
+              <motion.img 
+                whileTap={{ scale: 0.6 }}
+                src={Avatar} 
+                className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full" 
+                alt="userprofile"
+                onClick={login} />
+              </div>
         </div>
       </div>
-        
-
-            
-            
-    
-        
-
 
         {/* small screens */}
         <div className="flex md:hidden w-full h-full"></div>
