af5f87760ee9ada083127de4f6a727dca9593825
McRonaah <ronaldcheruiyot342@gmail.com>
2023-03-29 09:47:49 +0300

    :boom: created main container and create container :tada:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Route, Routes } from "react-router-dom";
 import { Header } from './components';
 
 const App = () => {
   return (
     <div className="w-screen h-auto flex  flex-col" >
         <Header />
+        <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
+          <Routes>
+            <Route path="/*" element={<MainContainer />} />
+            <Route path="/createItem" element={<CreateContainer />} />
+          </Routes>
+        </main>
     </div>
   );
 }
