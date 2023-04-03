8bbaace05edb09476e3528a6b5b45f8430d74720
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-03 12:17:11 +0300

    :wheelchair: updated logout link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Logo from '../img/logo.png';
-import { MdShoppingBasket } from "react-icons/md";
+
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md";
 import { motion } from 'framer-motion';
 
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
@@ -8,7 +10,6 @@ import { app } from "../firebase.config";
 
 
 import Avatar from "../img/avatar.png";
-import { Link } from 'react-router-dom';
 
 import { useStateValue } from "../context/StateProvider";
 import { actionType } from "../context/reducer";
@@ -85,9 +86,17 @@ const Header = () => {
                alt="userprofile"
                onClick={login}/>
 
-                <dir className="w-40 bg-primary shadow-xl rounded-lg flex flex-col absolute top-12 right-0 px-4 py-2">
-                  <p>New Item</p>
-                  <p>Logout</p>
+                <dir className="w-40 bg-primary shadow-xl rounded-lg flex flex-col absolute top-12 right-0 ">
+                  {
+                    user && user.email === "ronaldcheruiyot342@gmail.com" && (
+                    <Link to={"/createItem"}>
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
+                  transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd /></p>
+                    </Link>
+                    )
+                  }
+                  <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
+                  transition-all duration-100 ease-in-out text-textColor text-base">Logout <MdLogout /> </p>
                 </dir>
               </div>
         </div>
