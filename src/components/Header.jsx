6fa94c79fa4c657c96dcb75c41448daaf3af5e1c
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-03 12:41:46 +0300

    :card_file_box: update opacity animate on dropdown menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,21 +21,21 @@ const Header = () => {
 
   const [{ user }, dispatch] = useStateValue();
 
-  const [isMenu, setIsMenu] = useState(false)
+  const [isMenu, setIsMenu] = useState(false);
 
   const login = async () => {
     if (!user) {
-    const { user: { refreshToken, providerData }
-  } = await signInWithPopup(firebaseAuth, provider);
-   dispatch({
-    type: actionType.SET_USER,
-    user: providerData[0],
-   });
-   localStorage.setItem("user", JSON.stringify(providerData[0]));
-  }
-  // } else {
-  //   setIsMenu(!isMenu);
-  // }
+      const {
+        user: { refreshToken, providerData },
+      } = await signInWithPopup(firebaseAuth, provider);
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0],
+      });
+      localStorage.setItem("user", JSON.stringify(providerData[0]));
+    } else {
+      setIsMenu(!isMenu);
+    }
   };
 
 
@@ -90,18 +90,24 @@ const Header = () => {
 
                 {
                   isMenu && (
-                    <div className="w-40 bg-primary shadow-xl rounded-lg flex flex-col absolute top-12 right-0 ">
+                    <motion.div
+                     initial={{opacity: 0, scale : 0.6}}
+                     animate={{opacity: 1, scale : 1}}
+                     exit={{opacity: 0, scale : 0.6}} 
+                    className="w-40 bg-primary shadow-xl rounded-lg flex flex-col absolute top-12 right-0 ">
                     {
                       user && user.email === "ronaldcheruiyot342@gmail.com" && (
                       <Link to={"/createItem"}>
                       <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
-                    transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd /></p>
+                    transition-all duration-100 ease-in-out text-textColor text-base">
+                      New Item <MdAdd /></p>
                       </Link>
                       )
                     }
                     <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
-                    transition-all duration-100 ease-in-out text-textColor text-base">Logout <MdLogout /> </p>
-                  </div>
+                    transition-all duration-100 ease-in-out text-textColor text-base">
+                      Logout <MdLogout /> </p>
+                  </motion.div>
                   )
                 }
                
