c95771699aa91bc3a1e0ea4ee1bf735abcc3d1ea
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-03 13:33:36 +0300

    :speech_balloon: small screen responsiveness :lipsstick:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -49,6 +49,7 @@ const Header = () => {
           <p className="text-headingColor text-xl font-bold">YumDrop</p>
         </Link>
         <div className="flex items-center gap-8">
+
           <motion.ul initial={{opacity: 0, x : 200}} 
           animate={{opacity: 1, x : 0}}
           exit={{opacity: 0, x : 200}}
@@ -108,7 +109,8 @@ const Header = () => {
                       )
                     }
                     <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
-                    transition-all duration-100 ease-in-out text-textColor text-base">
+                    transition-all duration-100 ease-in-out text-textColor text-base"
+                    onClick={logout}>
                       Logout <MdLogout /> </p>
                   </motion.div>
                   )
@@ -119,7 +121,8 @@ const Header = () => {
       </div>
 
         {/* small screens */}
-        <div className="flex md:hidden w-full h-full">
+
+        <div className="flex items-center justify-between md:hidden w-full h-full">
         <Link to={"/"} className="flex items-center gap-2">
           <img src={Logo} className="w-8 object-cover" alt="logo" />
           <p className="text-headingColor text-xl font-bold">YumDrop</p>
@@ -148,8 +151,37 @@ const Header = () => {
                       </Link>
                       )
                     }
-                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
-                    transition-all duration-100 ease-in-out text-textColor text-base">
+
+            <ul
+            className="flex flex-col ">
+                <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2"
+                //   onClick={() => setIsMenu(false)}
+                >
+                  Home
+                </li>
+                <li
+                  className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2"
+                //   onClick={() => setIsMenu(false)}
+                >
+                  Menu
+                </li>
+                <li
+                  className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2"
+                //   onClick={() => setIsMenu(false)}
+                >
+                  About Us
+                </li>
+                <li
+                  className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2"
+                //   onClick={() => setIsMenu(false)}
+                >
+                  Service
+                </li>
+              </ul>
+
+                    <p className="m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-gray-300 transition-all 
+                    duration-100 ease-in-out text-textColor text-base"
+                    onClick={logout}>
                       Logout <MdLogout /> </p>
                   </motion.div>
                   )
