2832c82a94d2fbae5471d9fcd0f835036ff767e0
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-03 13:13:29 +0300

    :loud_sound: update responsiveness

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import { AnimatePresence } from 'framer-motion';
-import {  Header, MainContainer, CreateContainer } from './components';
+import { CreateContainer, Header, MainContainer } from './components';
 
 const App = () => {
   return (
-   <AnimatePresence>
+   <AnimatePresence wait>
      <div className="w-screen h-auto flex  flex-col" >
         <Header />
         <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,7 +40,7 @@ const Header = () => {
 
 
   return (
-    <header className="fixed z-50 w-screen p-6 px-16"> 
+    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16"> 
             {/* large screen */}
     {/* <div className="fixed z-50 w-screen p-6 px-16"> */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
@@ -49,7 +49,10 @@ const Header = () => {
           <p className="text-headingColor text-xl font-bold">YumDrop</p>
         </Link>
         <div className="flex items-center gap-8">
-          <ul className="flex items-center gap-8">
+          <motion.ul initial={{opacity: 0, x : 200}} 
+          animate={{opacity: 1, x : 0}}
+          exit={{opacity: 0, x : 200}}
+            className="flex items-center gap-8">
                 <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
                 //   onClick={() => setIsMenu(false)}
                 >
@@ -73,7 +76,7 @@ const Header = () => {
                 >
                   Service
                 </li>
-              </ul>
+              </motion.ul>
                 <div className="relative flex items-center justify-center">
                   <MdShoppingBasket className="text-textColor text-2xl ml-8 cursor-pointer" />
                     <div className=" absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
@@ -116,7 +119,44 @@ const Header = () => {
       </div>
 
         {/* small screens */}
-        <div className="flex md:hidden w-full h-full"></div>
+        <div className="flex md:hidden w-full h-full">
+        <Link to={"/"} className="flex items-center gap-2">
+          <img src={Logo} className="w-8 object-cover" alt="logo" />
+          <p className="text-headingColor text-xl font-bold">YumDrop</p>
+        </Link>
+        <div className="relative">
+              <motion.img 
+               whileTap={{ scale: 0.6 }}
+               src={user ? user.photoURL : Avatar}
+               className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+               alt="userprofile"
+               onClick={login}/>
+
+                {
+                  isMenu && (
+                    <motion.div
+                     initial={{opacity: 0, scale : 0.6}}
+                     animate={{opacity: 1, scale : 1}}
+                     exit={{opacity: 0, scale : 0.6}} 
+                    className="w-40 bg-primary shadow-xl rounded-lg flex flex-col absolute top-12 right-0 ">
+                    {
+                      user && user.email === "ronaldcheruiyot342@gmail.com" && (
+                      <Link to={"/createItem"}>
+                      <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
+                    transition-all duration-100 ease-in-out text-textColor text-base">
+                      New Item <MdAdd /></p>
+                      </Link>
+                      )
+                    }
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
+                    transition-all duration-100 ease-in-out text-textColor text-base">
+                      Logout <MdLogout /> </p>
+                  </motion.div>
+                  )
+                }
+               
+              </div>
+        </div>
     </header>
   );
 }
