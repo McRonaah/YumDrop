92685b4cfa058398d1fcf8742661117b02de2c38
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-03 12:31:04 +0300

    :bulb: update menu when user is login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../img/logo.png';
 
 import { Link } from 'react-router-dom';
@@ -20,6 +20,8 @@ const Header = () => {
   const provider = new GoogleAuthProvider();
 
   const [{ user }, dispatch] = useStateValue();
+
+  const [isMenu, setIsMenu] = useState(false)
 
   const login = async () => {
     if (!user) {
@@ -86,18 +88,23 @@ const Header = () => {
                alt="userprofile"
                onClick={login}/>
 
-                <dir className="w-40 bg-primary shadow-xl rounded-lg flex flex-col absolute top-12 right-0 ">
-                  {
-                    user && user.email === "ronaldcheruiyot342@gmail.com" && (
-                    <Link to={"/createItem"}>
+                {
+                  isMenu && (
+                    <div className="w-40 bg-primary shadow-xl rounded-lg flex flex-col absolute top-12 right-0 ">
+                    {
+                      user && user.email === "ronaldcheruiyot342@gmail.com" && (
+                      <Link to={"/createItem"}>
+                      <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
+                    transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd /></p>
+                      </Link>
+                      )
+                    }
                     <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
-                  transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd /></p>
-                    </Link>
-                    )
-                  }
-                  <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
-                  transition-all duration-100 ease-in-out text-textColor text-base">Logout <MdLogout /> </p>
-                </dir>
+                    transition-all duration-100 ease-in-out text-textColor text-base">Logout <MdLogout /> </p>
+                  </div>
+                  )
+                }
+               
               </div>
         </div>
       </div>
