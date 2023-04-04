0e891f180a3dc0aba66ab6f69facc12302e94222
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-04 10:13:04 +0300

    :ambulance: main container responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -132,10 +132,23 @@ const Header = () => {
         {/* small screens */}
 
         <div className="flex items-center justify-between md:hidden w-full h-full">
+
+
+        <div className="relative flex items-center justify-center">
+                  <MdShoppingBasket className="text-textColor text-2xl ml-8 cursor-pointer" />
+                    <div className=" absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+                      <p className="text-xs text-white font-semibold">2</p>
+                    </div>
+                </div>
+
+                
         <Link to={"/"} className="flex items-center gap-2">
           <img src={Logo} className="w-8 object-cover" alt="logo" />
           <p className="text-headingColor text-xl font-bold">YumDrop</p>
         </Link>
+
+            
+
         <div className="relative">
               <motion.img 
                whileTap={{ scale: 0.6 }}
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const MainContainer = () => {
   return (
-    <div>MainContainer</div>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-2" >
+      <div className="p-4 bg-blue-400 flex-1"></div>
+      <div className="p-4 bg-blue-400 flex-1"></div>
+    </div>
   );
 }
 
