6d3a67ae32b8be0f9aed8c2f6f9e8f9038b19113
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-04 12:25:42 +0300

    :white_check_mark:order button

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -5,7 +5,7 @@ import Delivery from '../img/delivery.png';
 const MainContainer = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-2" >
-      <div className="py-2 flex-1 flex flex-col items-start justify-center md:items-center">
+      <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
         <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
           <p className="text-base text-orange-500 font-semibold">Motorcycle Delivery </p>
           <div className="w-8 h-8 rounded-full overflow-hidden drop-shadow-xl">
@@ -14,6 +14,20 @@ const MainContainer = () => {
                  alt="bodaboda-delivery" />
           </div>
         </div>
+        <p className="text-[2.5rem] font-bold tracking-wide text-headingColor ">
+          The fastest and cheapest delivery in 
+          <span className="text-orange-600 text-[3rem]">Your Estate</span>.
+        </p>
+        <p className="text-base text-textColor text-center md:text-left">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc semper sagittis velit a mollis.
+           Duis convallis odio non ligula consectetur, non sodales lacus consectetur. Nullam convallis euismod
+            lacinia. Donec ullamcorper augue in ipsum dapibus sagittis. Suspendisse potenti.</p>
+
+          <button 
+          type="button" className="bg-gradient-to-br from-orange-400 to-orange-500 w-full 
+          md:w-auto px-4 py-2
+          rounded-lg hover:shadow-lg transition-all ease-in-out duration-100" >
+            Order Now</button>
       </div>
       <div className="p-4 bg-blue-400 flex-1"></div>
     </div>
