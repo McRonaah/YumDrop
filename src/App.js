7ab7806c70fcd4e8fe1512a4927645b74a103de1
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-04 15:28:19 +0300

    :wrench useEffect update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Routes } from "react-router-dom";
 import { AnimatePresence } from 'framer-motion';
 import { CreateContainer, Header, MainContainer } from './components';
 import { useStateValue } from "./context/StateProvider";
 import { getAllFoodItems } from "./utils/firebaseFunctions";
+import { actionType } from "./context/reducer";
+
 
 const App = () => {
   const [{ foodItems }, dispatch] = useStateValue();
 
   const fetchData = async () => {
     await getAllFoodItems().then((data) => {
-      // dispatch({
-        // type: actionType.SET_FOOD_ITEMS,
-        // foodItems: data,
-        console.log(data)
-      // });
+      dispatch({
+        type: actionType.SET_FOOD_ITEMS,
+        foodItems: data,
+      });
     });
   };
 
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
    <AnimatePresence wait>
