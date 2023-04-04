a17a5c6cee2ed2e3dc0432fc1e000c7a83d6c858
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-04 15:02:59 +0300

    :monocle_face: image upload functionality :dizzy:

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -10,15 +10,16 @@ import {
 } from "react-icons/md";
 import { categories } from "../utils/data";
 import Loader from "./Loader";
-// import {
-//   deleteObject,
-//   getDownloadURL,
-//   ref,
-//   uploadBytesResumable,
-// } from "firebase/storage";
-// import { storage } from "../firebase.config";
-// import { actionType } from "../context/reducer";
-// import { useStateValue } from "../context/StateProvider";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { storage } from "../firebase.config";
+import { getAllFoodItems, saveItem } from "../utils/firebaseFunctions";
+import { actionType } from "../context/reducer";
+import { useStateValue } from "../context/StateProvider";
 
 const CreateContainer = () => {
   const [title, setTitle] = useState("");
@@ -33,58 +34,101 @@ const CreateContainer = () => {
   const [{ foodItems }, dispatch] = useStateValue();
 
   const uploadImage = (e) => {
-    // setIsLoading(true);
-    // const imageFile = e.target.files[0];
-    // const storageRef = ref(storage, `Images/${Date.now()}-${imageFile.name}`);
-    // const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    setIsLoading(true);
+    const imageFile = e.target.files[0];
+    const storageRef = ref(storage, `Images/${Date.now()}-${imageFile.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
 
-    // uploadTask.on(
-    //   "state_changed",
-    //   (snapshot) => {
-    //     const uploadProgress =
-    //       (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    //   },
-    //   (error) => {
-    //     console.log(error);
-    //     setFields(true);
-    //     setMsg("Error while uploading : Try AGain 🙇");
-    //     setAlertStatus("danger");
-    //     setTimeout(() => {
-    //       setFields(false);
-    //       setIsLoading(false);
-    //     }, 4000);
-    //   },
-    //   () => {
-    //     getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-    //       setImageAsset(downloadURL);
-    //       setIsLoading(false);
-    //       setFields(true);
-    //       setMsg("Image uploaded successfully 😊");
-    //       setAlertStatus("success");
-    //       setTimeout(() => {
-    //         setFields(false);
-    //       }, 4000);
-    //     });
-    //   }
-    // );
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const uploadProgress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      },
+      (error) => {
+        console.log(error);
+        setFields(true);
+        setMsg("Error while uploading : Try AGain 🙇");
+        setAlertStatus("danger");
+        setTimeout(() => {
+          setFields(false);
+          setIsLoading(false);
+        }, 4000);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageAsset(downloadURL);
+          setIsLoading(false);
+          setFields(true);
+          setMsg("Image uploaded successfully 😊");
+          setAlertStatus("success");
+          setTimeout(() => {
+            setFields(false);
+          }, 4000);
+        });
+      }
+    );
   };
 
   const deleteImage = () => {
-    // setIsLoading(true);
-    // const deleteRef = ref(storage, imageAsset);
-    // deleteObject(deleteRef).then(() => {
-    //   setImageAsset(null);
-    //   setIsLoading(false);
-    //   setFields(true);
-    //   setMsg("Image deleted successfully 😊");
-    //   setAlertStatus("success");
-    //   setTimeout(() => {
-    //     setFields(false);
-    //   }, 4000);
-    // });
+    setIsLoading(true);
+    const deleteRef = ref(storage, imageAsset);
+    deleteObject(deleteRef).then(() => {
+      setImageAsset(null);
+      setIsLoading(false);
+      setFields(true);
+      setMsg("Image deleted successfully 😊");
+      setAlertStatus("success");
+      setTimeout(() => {
+        setFields(false);
+      }, 4000);
+    });
   };
 
-  
+  const saveDetails = () => {
+    setIsLoading(true);
+    try {
+      if (!title || !calories || !imageAsset || !price || !category) {
+        setFields(true);
+        setMsg("Required fields can't be empty");
+        setAlertStatus("danger");
+        setTimeout(() => {
+          setFields(false);
+          setIsLoading(false);
+        }, 4000);
+      } else {
+        const data = {
+          id: `${Date.now()}`,
+          title: title,
+          imageURL: imageAsset,
+          category: category,
+          calories: calories,
+          qty: 1,
+          price: price,
+        };
+        saveItem(data);
+        setIsLoading(false);
+        setFields(true);
+        setMsg("Data Uploaded successfully 😊");
+        setAlertStatus("success");
+        setTimeout(() => {
+          setFields(false);
+        }, 4000);
+        clearData();
+      }
+    } catch (error) {
+      console.log(error);
+      setFields(true);
+      setMsg("Error while uploading : Try AGain 🙇");
+      setAlertStatus("danger");
+      setTimeout(() => {
+        setFields(false);
+        setIsLoading(false);
+      }, 4000);
+    }
+
+    fetchData();
+  };
 
   const clearData = () => {
     setTitle("");
@@ -180,7 +224,11 @@ const CreateContainer = () => {
               ) : (
                 <>
                   <div className="relative h-full">
-                   
+                    <img
+                      src={imageAsset}
+                      alt="uploaded image"
+                      className="w-full h-full object-cover"
+                    />
                     <button
                       type="button"
                       className="absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md  duration-500 transition-all ease-in-out"
