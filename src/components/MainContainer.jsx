d922c0518b8b0e1a0a416cbd26149d6f807b5d34
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-05 14:54:39 +0300

    :clown_face: mock cart file in main component

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -20,39 +20,43 @@ const MainContainer = () => {
   useEffect(() => {}, [scrollValue, cartShow]);
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center ">
-      <HomeContainer />
-      <section  className="w-full my-6">
+    <HomeContainer />
+
+    <section className="w-full my-6">
       <div className="w-full flex items-center justify-between">
-          <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100">
-            Our fresh & healthy fruits
-          </p>
-      <div className="hidden md:flex gap-3 items-center">
-            <motion.div
-              whileTap={{ scale: 0.75 }}
-              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer  hover:shadow-lg flex items-center justify-center"
-              onClick={() => setScrollValue(-200)}
-            >
-              <MdChevronLeft className="text-lg text-white" />
-            </motion.div>
-            <motion.div
-              whileTap={{ scale: 0.75 }}
-              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"
-              onClick={() => setScrollValue(200)}
-            >
-              <MdChevronRight className="text-lg text-white" />
-            </motion.div>
-          </div>
+        <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100">
+          Our fresh & healthy fruits
+        </p>
+
+        <div className="hidden md:flex gap-3 items-center">
+          <motion.div
+            whileTap={{ scale: 0.75 }}
+            className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer  hover:shadow-lg flex items-center justify-center"
+            onClick={() => setScrollValue(-200)}
+          >
+            <MdChevronLeft className="text-lg text-white" />
+          </motion.div>
+          <motion.div
+            whileTap={{ scale: 0.75 }}
+            className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"
+            onClick={() => setScrollValue(200)}
+          >
+            <MdChevronRight className="text-lg text-white" />
+          </motion.div>
         </div>
-        <ListComponent 
+      </div>
+      <ListComponent
         scrollValue={scrollValue}
         flag={true}
         data={foodItems?.filter((n) => n.category === "fruits")}
-        />
-        <MenuComponent />
-        <Cart />
-      </section>
-    </div>
-  );
-}
+      />
+    </section>
+
+    <MenuComponent />
+
+    {cartShow && <Cart />}
+  </div>
+);
+};
 
 export default MainContainer;
