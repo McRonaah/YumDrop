d7d695c38a91b64a31c62b125aae7e9234ac54e4
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-05 16:26:48 +0300

    :lock: added tailwind scroll-bar as plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       width: {
@@ -60,5 +57,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("tailwind-scrollbar")],
+};
