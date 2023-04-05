a78ab835d2e0725a611ab5da129e33611dc6a4cd
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-05 12:22:09 +0300

    :white_check_mark: setting useeffect and onclick scroll funtionality

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 
 import HomeContainer from './HomeContainer';
+
+import { useStateValue } from "../context/StateProvider";
+
 
 import { motion } from "framer-motion";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import ListComponent from './ListComponent';
 
 const MainContainer = () => {
+
+  const [{ foodItems, cartShow }] = useStateValue();
+  const [scrollValue, setScrollValue] = useState(0);
+
+  useEffect(() => {}, [scrollValue, cartShow]);
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center ">
       <HomeContainer />
@@ -19,23 +27,23 @@ const MainContainer = () => {
             <motion.div
               whileTap={{ scale: 0.75 }}
               className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer  hover:shadow-lg flex items-center justify-center"
-              // onClick={() => setScrollValue(-200)}
+              onClick={() => setScrollValue(-200)}
             >
               <MdChevronLeft className="text-lg text-white" />
             </motion.div>
             <motion.div
               whileTap={{ scale: 0.75 }}
               className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"
-              // onClick={() => setScrollValue(200)}
+              onClick={() => setScrollValue(200)}
             >
               <MdChevronRight className="text-lg text-white" />
             </motion.div>
           </div>
         </div>
         <ListComponent 
-        // scrollValue={scrollValue}
+        scrollValue={scrollValue}
         flag={true}
-        // data={foodItems?.filter((n) => n.category === "fruits")}
+        data={foodItems?.filter((n) => n.category === "fruits")}
         />
 
       </section>
