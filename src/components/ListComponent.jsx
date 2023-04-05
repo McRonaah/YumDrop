d68bedffd7753278c89fa44d4891f69c840bb7a2
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-05 11:57:59 +0300

    :lipstick: stling List component

--- src/components/ListComponent.jsx
+++ src/components/ListComponent.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const ListComponent = () => {
+const ListComponent = ({ flag,}) => {
   return (
-    <div>ListComponent</div>
-  )
-}
+    <div className={`w-full flex items-center gap-3  my-12 scroll-smooth  ${
+        flag
+          ? "overflow-x-scroll scrollbar-none"
+          : "overflow-x-hidden flex-wrap justify-center"
+      }`}>ListComponent
+      <div className="w-full flex items-center justify-between">
+        
+      </div>
+      </div>
+  );
+};
 
 export default ListComponent;
