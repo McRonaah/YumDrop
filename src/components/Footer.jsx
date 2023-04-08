e831185442afeb3d8dc1a9bdac28a49043418256
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-08 20:49:04 +0300

    :lipstick: styling footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,27 +4,24 @@ import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="container">
-        <div className="footer-content">
-          <div className="social-media-icons">
-            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faFacebook} />
-            </a>
-            <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
-            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faInstagram} />
-            </a>
-          </div>
-          <div className="footer-text">
-            <p>© 2023 YumDrop. All Rights Reserved.</p>
-          </div>
-        </div>
+    <footer className="bg-gray-900 text-gray-400 py-4">
+    <div className="container mx-auto flex justify-between items-center">
+      <div className="social-media-icons flex space-x-6">
+        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faFacebook} className="text-gray-400 hover:text-white transition-all duration-300" />
+        </a>
+        <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faTwitter} className="text-gray-400 hover:text-white transition-all duration-300" />
+        </a>
+        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faInstagram} className="text-gray-400 hover:text-white transition-all duration-300" />
+        </a>
       </div>
-    </footer>
-  );
+      <div className="footer-text">
+        <p className="text-sm">&copy; 2023 YumDrop.  All Rights Reserved.</p>
+      </div>
+    </div>
+  </footer>
+);
 };
-
 export default Footer;
