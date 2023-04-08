3a3fae74d8c0c736b9d99fa95d21597f8680130a
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-08 20:52:51 +0300

    :art: added linkedIn icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faTwitter, faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-gray-400 py-4">
+    <footer className="bg-gray-700 text-gray-400 py-4">
     <div className="container mx-auto flex justify-between items-center">
       <div className="social-media-icons flex space-x-6">
         <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faFacebook} className="text-gray-400 hover:text-white transition-all duration-300" />
+          <FontAwesomeIcon icon={faLinkedin} className="text-gray-400 hover:text-white transition-all duration-300" />
         </a>
         <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faTwitter} className="text-gray-400 hover:text-white transition-all duration-300" />
         </a>
         <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faInstagram} className="text-gray-400 hover:text-white transition-all duration-300" />
+        </a>
+        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faFacebook} className="text-gray-400 hover:text-white transition-all duration-300" />
         </a>
       </div>
       <div className="footer-text">
