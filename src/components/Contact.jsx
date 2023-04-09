bd96e51cb32396b856ba821bc21f770587ee788d
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-09 16:56:02 +0300

    :white_check_mark: contact page fit image

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import photo from "../img/WhatsApp Image 2023-04-09 at 16.26.55.jpeg";
 
 
 function Contact() {
   return (
-    <div className="flex flex-wrap rounded bg-[url('file:///home/ronald/amplification/angular/My_Profile/src/assets/images/background-images/diego-ph-fIq0tET6llw-unsplash.jpg')] bg-cover bg-no-repeat ">
-      <div className="w-full md:w-1/2 p-4">
+    <div className="flex flex-wrap rounded bg-[url('file:///home/ronald/amplification/angular/My_Profile/src/assets/images/background-images/diego-ph-fIq0tET6llw-unsplash.jpg')] bg-cover bg-no-repeat">
+      <div className="w-full md:w-1/2 p-4 h-1/4">
         <h2 className="text-xl font-bold mb-4">Contact Us</h2>
         <p className="mb-2">We'd love to hear from you!</p>
         <p>Don't hesitate to message us via what'sApp by clicking the icon
@@ -18,7 +19,7 @@ function Contact() {
             <a href="tel:+254111310907" target="_blank" rel="noopener noreferrer" 
             className="text-gray-400  hover:text-white transition-all duration-300"><i class="fa fa-phone"></i>+254111310907</a>
         </p>
-        <img src="https://via.placeholder.com/300x200" alt="Contact Us" className="mb-4" />
+        <img src={photo} alt="Contact Us" className=" h-96 w-80 mb-4 rounded " />
         <p>Thank you for taking the time to browse through the site shot an email&nbsp;
         <a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=ronaldcheruiyot342@gmail.com" target="_blank" rel="noopener noreferrer" 
         className="text-gray-400  hover:text-white transition-all duration-300"> 
@@ -26,8 +27,8 @@ function Contact() {
             &nbsp;and we will get back within 24 hrs.
         </p>
       </div>
-      <div className="w-full md:w-1/2 p-4">
-        <form className="bg-white rounded-lg shadow-md p-8">
+      <div className="w-full h-full md:w-1/2 p-4">
+        <form className="rounded-lg shadow-md p-8">
           <h2 className="text-xl font-bold mb-4">Get In Touch</h2>
           <p className="mb-4">Please fill out the form below and we'll get back to you shortly.</p>
           <div className="mb-4">
