7e31e123a490594ca0c07b01cbe09984a435a401
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-11 18:51:11 +0300

    :sparkles: setting up firebase storage

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,9 +3,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import photo from "../img/WhatsApp Image 2023-04-09 at 16.26.55.jpeg";
 
-import firebase from "firebase/app";
-import "firebase/database";
-
+// import firebase from 'firebase';
+import { collection, addDoc } from "firebase/firestore";
+import { database } from "../firebase.config";
 
 function Contact() {
   const [name, setName] = useState("");
@@ -13,30 +13,29 @@ function Contact() {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [message, setMessage] = useState("");
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
-    
-    const database = firebase.database();
-    database.ref("messages").push({
-      name: name,
-      email: email,
-      phoneNumber: phoneNumber,
-      message: message,
-    })
-      .then(() => {
-        alert("Thank you for your message!");
-        setName("");
-        setEmail("");
-        setPhoneNumber("");
-        setMessage("");
-      })
-      .catch((error) => {
-        console.error(error);
-        alert("An error occurred. Please try again later.");
+
+    try {
+      const docRef = await addDoc(collection(database, "messages"), {
+        name: name,
+        email: email,
+        phoneNumber: phoneNumber,
+        message: message,
       });
+      console.log("Document written with ID: ", docRef.id);
+      alert("Thank you for your message!");
+      setName("");
+      setEmail("");
+      setPhoneNumber("");
+      setMessage("");
+    } catch (error) {
+      console.error("Error adding document: ", error);
+      alert("An error occurred. Please try again later.");
+    }
   }
   return (
-    <div className="flex flex-wrap rounded bg-[url('file:///home/ronald/amplification/angular/My_Profile/src/assets/images/background-images/diego-ph-fIq0tET6llw-unsplash.jpg')] bg-cover bg-no-repeat">
+    <div className="flex flex-wrap rounded bg-[url('https://pas-wordpress-media.s3.amazonaws.com/content/uploads/2017/10/idea-but-no-funding.jpg')] bg-cover bg-no-repeat">
       <div className="w-full md:w-1/2 p-4 h-1/4">
         <h2 className="text-xl font-bold mb-4">Contact Us</h2>
         <p className="mb-2">We'd love to hear from you!</p>
